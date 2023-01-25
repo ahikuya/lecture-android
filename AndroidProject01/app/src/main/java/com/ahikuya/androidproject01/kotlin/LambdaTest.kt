c96cbddbcba7d8ff489d87a43d0af767402b76a7
ahikuya <ahikuya@gmail.com>
2023-01-25 18:11:41 +0900

    고차함수 예제

--- AndroidProject01/app/src/main/java/com/ahikuya/androidproject01/kotlin/LambdaTest.kt
+++ AndroidProject01/app/src/main/java/com/ahikuya/androidproject01/kotlin/LambdaTest.kt
@@ -1,6 +1,22 @@
 package com.ahikuya.androidproject01.kotlin
 
+
+
+// 고차함수 정의
+// 매개변수로 함수를 입력 받고 함수를 반환한다.
+fun validSize(width: (Int) -> Boolean): () -> String{
+    val result = if(width(50)){
+        "valid"
+    }else{
+        "invalid"
+    }
+    return {result}
+}
+
 fun main() {
     val calcSize = {width:Int, height: Int -> width * height}       // 람다 함수를 선언하고 변수에 대입
     calcSize(20, 20)        // 람다 함수 실행
+
+    val validSize = validSize({no -> no > 0})
+    println(validSize())
 }
