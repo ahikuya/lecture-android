d5ca6e1b19b3e91f129c31568c78669897dbcfdd
ahikuya <ahikuya@gmail.com>
2023-01-25 17:04:12 +0900

    데이터 클래스 equals(), toString() 테스트

--- AndroidProject01/app/src/main/java/com/ahikuya/androidproject01/kotlin/DataClassTest.kt
+++ AndroidProject01/app/src/main/java/com/ahikuya/androidproject01/kotlin/DataClassTest.kt
@@ -1,15 +1,20 @@
 package com.ahikuya.androidproject01.kotlin
 
+
+// 일반 클래스 선언
 class MyBasicClass(val name:String, val age:Int)
 
+// 데이터 클래스 선언
 data class MyDataClass(val name:String, val age:Int)
 
 fun main() {
     val basic1 = MyBasicClass("01", 20)
     val basic2 = MyBasicClass("01", 20)
     println("basic equals = ${basic1.equals(basic2)}")
+    println("basic toString = ${basic1.toString()}")
 
     val data1 = MyDataClass("02", 30)
     val data2 = MyDataClass("02", 30)
-    println("basic equals = ${data1.equals(data2)}")
+    println("data1 equals = ${data1.equals(data2)}")
+    println("data1 toString = ${data1.toString()}")
 }
